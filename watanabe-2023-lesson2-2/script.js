7e9c500a9a5bdfad3360662527ca62a4ec856a2f
Tetsuya Mochizuki <temochiz@gmail.com>
2023-09-06 08:49:24 +0900

    modified save

--- watanabe-2023-lesson2-2/script.js
+++ watanabe-2023-lesson2-2/script.js
@@ -281,4 +281,4 @@ trials.timeline.push(blankscreen) ;
 // 実験の開始
 // ------------------------------------------------------------------------
 
-jsPsych.run([preload,enter_fullscreen,par_id,pre_hello,pre_trials,pre_bye,hello,trials,save_data,bye,exit_fullscreen]);
+jsPsych.run([preload,enter_fullscreen,par_id,pre_hello,pre_trials,pre_bye,hello,trials,bye, save_data,exit_fullscreen]);
